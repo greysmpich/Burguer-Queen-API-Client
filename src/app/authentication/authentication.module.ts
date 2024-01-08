6817e7bf52171685a179d58938d7a4a2fb0e17f5
GiselleLop <giselleLop@users.noreply.github.com>
2024-01-08 14:33:50 -0500

    maquetacion authentication y fuentes

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationComponent } from './authentication.component';
+//import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [
