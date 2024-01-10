9c14ac56a1f3cd1af81fbec5cb95eaf14caa7b98
GiselleLop <giselleLop@users.noreply.github.com>
2024-01-09 20:07:50 -0500

    test html

--- src/app/authentication/authentication.component.spec.ts
+++ src/app/authentication/authentication.component.spec.ts
@@ -23,4 +23,42 @@ describe('AuthenticationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it(`should have as title "Burguer Queen"`, () => {
+    const fixture = TestBed.createComponent(AuthenticationComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.title')?.textContent).toContain('Burguer Queen');
+  });
+
+  it(`should have a Logo`, () => {
+    const fixture = TestBed.createComponent(AuthenticationComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.logoBurgerQueen')?.tagName.toLowerCase()).toContain('img');
+  });
+  it(`should have a container for the credentials`, () => {
+    const fixture = TestBed.createComponent(AuthenticationComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.containerLogin')?.tagName.toLowerCase()).toContain('div');
+  });
+  it(`should have two inputs for email and password`, () => {
+    const fixture = TestBed.createComponent(AuthenticationComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('input').length).toBe(2);
+  });
+  
+  // it(`the label for password should have a button to show and hide the password`, () => {
+  //   const fixture = TestBed.createComponent(AuthenticationComponent);
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   const containerInputPassword = compiled.querySelector('.containerInputPassword');
+  //   expect(containerInputPassword).toBeTruthy();
+  //   const hideButton = containerInputPassword.querySelector('button.eyeHide');
+  //   expect(hideButton).toBeTruthy();
+  // });
+
+  it(`should have a button to Login`, () => {
+    const fixture = TestBed.createComponent(AuthenticationComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#buttonLogin')).toBeTruthy();;
+  });
+
 });
