bc05ebc95e40ca2cc5ee34602ee898297fc97085
greysmpich <greys.mpich@gmail.com>
2024-01-15 23:30:58 -0600

    tests(components): se agregaron tests a lunchAndDinnerComponent y a AuthenticatinService para subir % de branches

--- src/app/components/lunch-and-dinner-button/lunch-and-dinner-button.component.spec.ts
+++ src/app/components/lunch-and-dinner-button/lunch-and-dinner-button.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LunchAndDinnerButtonComponent } from './lunch-and-dinner-button.component';
+import { By } from '@angular/platform-browser';
 
 describe('LunchAndDinnerButtonComponent', () => {
   let component: LunchAndDinnerButtonComponent;
@@ -28,4 +28,24 @@ describe('LunchAndDinnerButtonComponent', () => {
     component.select();
     expect(emitSpy).toHaveBeenCalled();
   });
+
+  it('should return selected style when isSelected is true', () => {
+     component.isSelected = true;
+     fixture.detectChanges();
+
+     const buttonElement = fixture.debugElement.query(By.css('.lunchAndDinner')).nativeElement;
+
+     expect(buttonElement.style.backgroundColor).toBe('rgb(238, 106, 9)');
+     expect(buttonElement.style.color).toBe('white');
+  })
+
+  it('should return selected style when isSelected is false', () => {
+    component.isSelected = false;
+    fixture.detectChanges();
+
+    const buttonElement = fixture.debugElement.query(By.css('.lunchAndDinner')).nativeElement;
+
+    expect(buttonElement.style.backgroundColor).toBe('rgb(217, 217, 217)');
+    expect(buttonElement.style.color).toBe('black');
+ })
 });
