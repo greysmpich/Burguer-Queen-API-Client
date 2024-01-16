a75515b30a3aa6d78948e87f73d881d4a9a0a21a
greysmpich <greys.mpich@gmail.com>
2024-01-15 21:45:24 -0600

    tests(component): se agregaron los tests para la mitad de las funciones del WaiterComponent

---
+++ src/app/views/waiter/mockProductList.ts
@@ -0,0 +1,119 @@
+import { productInter } from "src/app/shared/interfaces/product";
+
+export const mockBreakfastMenu: productInter[] = [
+    {
+        "id": 1,
+        "name": "American coffee",
+        "price": 500,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/coffee.png",
+        "type": "Beverages",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 5,
+        "name": "Milk",
+        "price": 500,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/blob/main/resources/images/milk.png",
+        "type": "Beverages",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 9,
+        "name": "Ham and cheese sandwich",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/sandwich.png",
+        "type": "Breakfast",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 10,
+        "name": "Fried egg sandwich",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/fried-egg.png",
+        "type": "Breakfast",
+        "dateEntry": "2022-03-05 15:14:10"
+    }
+];
+
+export const mockLunchAndDinnerMenu: productInter[] = [
+    {
+        "id": 2,
+        "name": "Water 500ml",
+        "price": 500,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/water.png",
+        "type": "Beverages",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 6,
+        "name": "Lemon soda",
+        "price": 500,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/soda-lemon.png",
+        "type": "Beverages",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 7,
+        "name": "Blue soda",
+        "price": 500,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/soda-blue.png",
+        "type": "Beverages",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 11,
+        "name": "Classic burger",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/sandwich.png",
+        "type": "Lunch",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 12,
+        "name": "Bacon burger",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/bacon-burger.png",
+        "type": "Lunch",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 13,
+        "name": "Cheese burger",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/cheese-burger.png",
+        "type": "Lunch",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 14,
+        "name": "Double burger",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/double-burger.png",
+        "type": "Lunch",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 18,
+        "name": "Salad",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/salad.png",
+        "type": "Sides",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 19,
+        "name": "Fries",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/french-fries.png",
+        "type": "Sides",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 20,
+        "name": "Spicy fries",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/french-potatoes.png",
+        "type": "Sides",
+        "dateEntry": "2022-03-05 15:14:10"
+    }
+]
--- src/app/views/waiter/waiter.component.spec.ts
+++ src/app/views/waiter/waiter.component.spec.ts
@@ -2,15 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { WaiterComponent } from './waiter.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { OrdersService } from 'src/app/services/orders/orders.service';
+import { mockBreakfastMenu, mockLunchAndDinnerMenu } from './mockProductList';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { BreakfastButtonComponent } from 'src/app/components/breakfast-button/breakfast-button.component';
+import { LunchAndDinnerButtonComponent } from 'src/app/components/lunch-and-dinner-button/lunch-and-dinner-button.component';
+import { OrderSummaryComponent } from 'src/app/components/order-summary/order-summary.component';
+import { LogoutComponent } from 'src/app/components/logout/logout.component';
 
 describe('WaiterComponent', () => {
   let component: WaiterComponent;
   let fixture: ComponentFixture<WaiterComponent>;
+  let ordersService: jasmine.SpyObj<OrdersService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WaiterComponent ],  
-      imports: [HttpClientTestingModule, RouterTestingModule]
+      declarations: [ WaiterComponent, HeaderComponent, BreakfastButtonComponent, LunchAndDinnerButtonComponent, OrderSummaryComponent, LogoutComponent],  
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [ OrdersService ],
     })
     .compileComponents();
   });
@@ -18,10 +27,68 @@ describe('WaiterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WaiterComponent);
     component = fixture.componentInstance;
+    ordersService = TestBed.inject(OrdersService) as jasmine.SpyObj<OrdersService>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+
+
+
+
+
+  it('it should filter breakfast menu and set currentMenu ti breakfast', () => {
+    const filterBreakfastMenuSpy = spyOn(ordersService, 'filterBreakfastMenu');
+    const expectedBreakfastMenu = mockBreakfastMenu;
+    filterBreakfastMenuSpy.and.returnValue(expectedBreakfastMenu);
+    
+    component.filterMenus();
+    
+    expect(component.breakfastMenu).toEqual(expectedBreakfastMenu);
+    expect(filterBreakfastMenuSpy).toHaveBeenCalled();
+  });
+
+  it('it should filter lunch and dinner menu and set currentMenu ti lunch and dinner menu', () => {
+     const filterLunchAndDinnerMenuSpy = spyOn(ordersService, 'filterLunchAndDinnerMenu');
+    const expectedLunchAndDinnerMenuMenu = mockLunchAndDinnerMenu;
+    filterLunchAndDinnerMenuSpy.and.returnValue(expectedLunchAndDinnerMenuMenu);
+    
+    component.filterMenus();
+
+    expect(component.lunchAndDinnerMenu).toEqual(expectedLunchAndDinnerMenuMenu);
+    expect(filterLunchAndDinnerMenuSpy).toHaveBeenCalled();
+  });
+
+  it('it should handeled lunch and dinner selection', () => {
+   const showLuncAhdnDinnerMenuSpy = spyOn(component, 'showLunchAndDinnerMenu');
+    component.handleLunchAndDinnerSelection();
+
+    expect(component.isLunchAndDinnerSelected).toBeTruthy();
+    expect(component.isBreakfastSelected).toBeFalsy();
+    expect(showLuncAhdnDinnerMenuSpy).toHaveBeenCalled();
+  });
+
+  it('it should update currentMenu based on lunch and dinner selection', () => {
+   
+    component.isLunchAndDinnerSelected = true;
+    component.lunchAndDinnerMenu = mockLunchAndDinnerMenu;
+  
+    component.showLunchAndDinnerMenu();
+    
+    expect(component.currentMenu).toEqual(component.lunchAndDinnerMenu);
+  });
+
+  it('it should show breakfast menu if lunch and dinner selection is false', () => {
+   
+    component.isLunchAndDinnerSelected = false;
+    component.breakfastMenu = mockBreakfastMenu;
+  
+    component.showLunchAndDinnerMenu();
+    
+    expect(component.currentMenu).toEqual(component.breakfastMenu);
   });
 });
