bc07fc1b669d600d632066cd3e087056e0bd932c
greysmpich <greys.mpich@gmail.com>
2024-01-19 12:01:22 -0600

    feat(components): se muestran en el OrderSummary los productos seleccionados y se envía la orden a la Api

--- src/app/components/order-summary/order-summary.component.ts
+++ src/app/components/order-summary/order-summary.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { OrdersService } from 'src/app/services/orders/orders.service';
+import { productInter } from 'src/app/shared/interfaces/product';
+import { Order, orderedProducts } from 'src/app/shared/interfaces/order';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-order-summary',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-summary.component.css']
 })
 export class OrderSummaryComponent implements OnInit {
+  @Input() product: productInter | undefined;
+  orderedProducts: orderedProducts[] = [];
+  private productSubscription: Subscription;
 
-  constructor() { }
+  constructor(private orderService: OrdersService) { 
+    this.productSubscription = this.orderService.getClickedProduct().subscribe(product => {
+      if(product) {
+        this.onProductClicked(product)
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onProductClicked(product: productInter) {
+    // Añade el producto a la lista de productos ordenados
+    this.orderedProducts.push({ qty: 1, product });
+    console.log('Desde summary: PRODUCTOP AÑADIDO A LA ORDEN', this.orderedProducts);
+  }
+
+  onSendOrderClick() {
+    // Envía la orden a la API
+    const order: Order = {
+      client: 'Nombre del Cliente', // Puedes obtener esto dinámicamente
+      products: this.orderedProducts,
+      status: 'Pending',  // Puedes establecer el estado como necesario
+      dataEntry: new Date(),
+      id: 0  // Puedes establecer un valor inicial para el ID
+    };
+
+    this.orderService.postOrder(order).subscribe((response => {
+      console.log('Orden enviada exitosamente', response);
+    }),
+    );
+
+    this.orderedProducts =  [];
+  }
+
+  ngOnDestroy() {
+    // Asegúrate de desuscribirte para evitar pérdidas de memoria
+    this.productSubscription.unsubscribe();
+  }
 }
