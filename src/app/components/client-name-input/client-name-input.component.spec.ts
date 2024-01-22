b1ea99a8b1f4d89697eb0ceccf6a7b8e00e3f579
GiselleLop <giselleLop@users.noreply.github.com>
2024-01-22 15:04:12 -0500

    chore: importaciones para test

--- src/app/components/client-name-input/client-name-input.component.spec.ts
+++ src/app/components/client-name-input/client-name-input.component.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ClientNameInputComponent } from './client-name-input.component';
-
+import { RouterTestingModule } from '@angular/router/testing';
 describe('ClientNameInputComponent', () => {
   let component: ClientNameInputComponent;
   let fixture: ComponentFixture<ClientNameInputComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ClientNameInputComponent ]
+      declarations: [ ClientNameInputComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
   });
