a88a2924f6a8ca034049093551043b8eadfb7f38
greysmpich <greys.mpich@gmail.com>
2024-01-25 12:15:18 -0600

    feat(components): se muestran órdenes con status Pending y Delivering y se actualiza el estado a Delivered en WaiterOrdersView

--- src/app/components/orders-pending-delivering/orders-pending-delivering.component.ts
+++ src/app/components/orders-pending-delivering/orders-pending-delivering.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from 'src/app/services/orders/orders.service';
+import { Order } from 'src/app/shared/interfaces/order';
+import { KitchenServiceService } from 'src/app/services/kitchen/kitchen-service.service';
 
 @Component({
   selector: 'app-orders-pending-delivering',
@@ -6,10 +9,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders-pending-delivering.component.css']
 })
 export class OrdersPendingDeliveringComponent implements OnInit {
+deliveringPendingList: Order[] = [];
+selectedOrderIndex: Order | null = null;
+selectedOrder: Order | null = null
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private ordersService: OrdersService, private kitchenService: KitchenServiceService) { 
+    // this.subscription = this.ordersService.clickedOrderDelivered$.subscribe(resp => {
+    //   this.selectedOrder = resp;      console.log(this.selectedOrder, ' show order');
+    // });
   }
 
+  ngOnInit(): void {
+    this.loadWaiterOrdersList()
+    this.ordersService.orderUpdated$.subscribe(() => {
+      this.loadWaiterOrdersList();
+    })
+
+  }
+
+  loadWaiterOrdersList() {
+    this.ordersService. getPendingDeliveringOrders().subscribe((resp => {
+      console.log('get order list ', resp);
+      this.deliveringPendingList = resp;
+      this.sortOrderByStatus();
+      console.log(this.deliveringPendingList);
+    })
+    );
+  }
+
+  onOrderClick(order: Order): void {
+    this.selectedOrderIndex = order;
+    this.ordersService.setOrderToDelivered(order);
+    this.selectedOrder = order;
+    console.log('Desde el order pending Waiter/orders', this.selectedOrder);
+  }
+
+  onDeliveredButtonClick(){
+    if(this.selectedOrder){
+      const orderId = this.selectedOrder.id;
+      const newStatus = 'Delivered';
+      
+      console.log(orderId, 'Selected Order ID!!!!');
+      
+     this.ordersService.updateOrderStatus(orderId, newStatus).subscribe(updatedOrder => {
+      this.ordersService.notifyOrderUpdated(updatedOrder.id)
+     })
+    }  
+  }
+
+  statusStyleWaiter(status: string): object {
+    if (status === 'Delivering') {
+      return { color: '#EE6A09' };
+    } else if (status === 'Pending') {
+      return { color: '#EE0909' };
+    }
+    return { color: '#3BBA26' }
+  }
+
+  sortOrderByStatus() {
+    this.deliveringPendingList.sort((a: Order, b: Order): number => {
+      if (a.status === b.status) {
+        return 0
+      }
+      return a.status === 'Pending' ? 1 : -1
+    })
+  }
 }
