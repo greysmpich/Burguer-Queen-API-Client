ff46ac85d56e17a229104a00d43ed6f55c627703
GiselleLop <giselleLop@users.noreply.github.com>
2024-01-26 13:25:43 -0500

    feat(components): test completos

--- src/app/views/waiter-order-status/waiter-order-status.component.spec.ts
+++ src/app/views/waiter-order-status/waiter-order-status.component.spec.ts
@@ -2,11 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { WaiterOrderStatusComponent } from './waiter-order-status.component';
+import { Router } from '@angular/router';
 
 describe('WaiterOrderStatusComponent', () => {
   let component: WaiterOrderStatusComponent;
   let fixture: ComponentFixture<WaiterOrderStatusComponent>;
-
+  let router: Router;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ WaiterOrderStatusComponent ],
@@ -18,10 +19,17 @@ describe('WaiterOrderStatusComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WaiterOrderStatusComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should back to menu', () => {
+    const navigateSpy = spyOn(router, 'navigateByUrl');
+    component.backToMenu()
+    expect(navigateSpy).toHaveBeenCalled();
+  });
+
 });
